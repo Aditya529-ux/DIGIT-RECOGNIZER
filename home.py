c8f60cc2738de946ec777456e486b242e437a02c
Aditya kumar sharma <adityasharma592004@gmail.com>
2025-02-15 00:18:00 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,22 +3,22 @@ import numpy as np
 from PIL import Image
 import pickle as pkl
 
-# Function to preprocess the uploaded image
+
 def preprocess_image(image_path):
-    # Open the image
-    img = Image.open(image_path).convert('L')  # Convert to grayscale
-    img = img.resize((28, 28))  # Resize to 28x28 pixels
-    img_array = np.array(img)  # Convert to NumPy array
-    img_array = 255 - img_array  # Invert colors (MNIST uses white digits on black background)
-    img_array = img_array / 255.0  # Normalize to range [0, 1]
-    img_array = img_array.reshape(1, 28, 28, 1)  # Add batch and channel dimensions
+    
+    img = Image.open(image_path).convert('L')  
+    img = img.resize((28, 28))  
+    img_array = np.array(img)  
+    img_array = 255 - img_array  
+    img_array = img_array / 255.0 
+    img_array = img_array.reshape(1, 28, 28, 1)  
     return img_array
 
 with open("cnn_model.pkl", "rb") as file:
         model = pkl.load(file)
 
 st.title("Digit Recogonizer")
-st.title("Made by Aditya")
+st.title("Made by CODE CRAFT")
 
 st.subheader("Upload image")
 img = st.file_uploader("1", label_visibility="collapsed")
@@ -30,9 +30,8 @@ if st.button("Submit"):
         prediction = model.predict(img)
         predict = int(np.argmax(prediction))
         st.subheader(f'Prediction: {predict}')
-        
-       
+        print("ALL RIGHTS ARE RESERVED BY CODE CRAFT")
+        print("MADE BY CODE CRAFT")
     else:
         st.subheader(f'Prediction: Null')
-st.title("All Rights Are Reserved")
-# python -m streamlit run home.py
+st.title("ALL RIGHTS ARE RESERVED BY CODE CRAFTE")
